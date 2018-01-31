c3f25f1720bc3928126d2e7fb4783ab61eee9604
Tyler Gabb <developer@polyprint.com>
2018-01-31 08:49:16 -0700

    reorganizing

--- ProgramaticAccessSource/DatabaseAccess.py
+++ ProgramaticAccessSource/DatabaseAccess.py
@@ -1,8 +1,12 @@
 import pymssql
 class DatabaseAccess:
     def __init__(self,server,user,password,database):
+        '''
         if(not '@' in user):
-            raise ValueError("Argument provided for `user` in wrong format, Must be in format user@server")
+            raise ValueError(
+                    "Argument provided for `user` in wrong format.\n"+
+                    "`user` ust be in format user@serverName")
+        '''
         self.conn = pymssql.connect(
             server=server,
             user=user,
@@ -15,16 +19,12 @@ class DatabaseAccess:
         return cursor
 
     def execute_rows(self,sqlCommand):
-        cursor = self.conn.cursor()
-        cursor.execute(sqlCommand)
+        cursor = self.execute_cursor(sqlCommand)
+        #cursor = self.conn.cursor()
+        #cursor.execute(sqlCommand)
         rows = [];
         row = cursor.fetchone()
         while row:
             rows.append(row)
             row = cursor.fetchone()
         return rows
-
-
-
-
-
