b32615319ea0976ede23be83f7116be4d0a44644
tylerjgabb <tylerjgabb@gmail.com>
2018-03-03 15:02:21 -0700

    forgot small procedure

--- TableValued_ParamsAndProcs/SQL/Inventory_CreateProcUploadMaterialScanTable.sql
+++ TableValued_ParamsAndProcs/SQL/Inventory_CreateProcUploadMaterialScanTable.sql
@@ -1,9 +1,9 @@
 ALTER PROC inventory_UploadMaterialScanTable(@table AS InventoryScanTable READONLY) AS BEGIN
-
+--https://stackoverflow.com/questions/2334712/how-do-i-update-from-a-select-in-sql-server
 UPDATE 
-	MaterialLocations
+	ML
 SET 
-	MaterialLocations.LOCATION = S.LOCATION
+	ML.LOCATION = S.LOCATION
 FROM 
 	MaterialLocations ML
 	INNER JOIN @table S ON S.BARCODE = ML.BARCODE
